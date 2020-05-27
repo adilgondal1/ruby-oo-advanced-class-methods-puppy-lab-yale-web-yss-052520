d13440b50661d81022a4f58c73f4c2ff8292209d
adilgondal1 <adil.gondal@yale.edu>
2020-05-27 16:19:22 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Add your code here
+class Dog
+    @@all = []
+
+    def initialize(name)
+        dog = self.new
+        @all << dog
+    end
+
+    def self.all()
+        @@all
+    end
+    
+end
